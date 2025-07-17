91e837fc68a31d98fbffbc14dc1afd44c8463135
patrick <siandjipatrick@yahoo.fr>
2025-07-17 15:27:26 +0200

    add the code to deploy n vm instance in environment

---
+++ infras/opentofu/main.tf
@@ -0,0 +1,41 @@
+locals {
+  all_vm_instances_list = flatten([
+    for env, config in var.Vm_pro_environment : [
+      for i in range(config.number_vm) : {
+        name  = "${env}_Server_${i+1}"
+        env   = env
+        index = i
+      }
+    ]
+  ])
+}
+
+locals {
+  all_vm_disks_name= flatten(
+                        [ for env, config in var.Vm_pro_environment:
+                                [for i in range(config.number_vm):
+                                    {
+                                        vm_disk_name = "${env}_${var.disk_name}_${i+1}"
+                                    }
+
+                                ]
+                        
+                        ])
+}  
+
+locals {
+  vm_disks_map = {
+    for item in local.all_vm_disks_name :
+    item.vm_disk_name => item
+  }
+}
+
+locals {
+  all_vm_instances_map = {
+    for vm in local.all_vm_instances_list :
+    vm.name => {
+      env   = vm.env
+      index = vm.index
+    }
+  }
+}
