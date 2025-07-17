e7ad0124e8536f84ada27d07eb347c57d5a53018
patrick <siandjipatrick@yahoo.fr>
2025-07-17 13:53:23 +0200

    rename terraform to opentofu

---
+++ infras/opentofu/vm.tf
@@ -0,0 +1,33 @@
+
+resource "libvirt_domain" "vm" {
+
+  for_each = {for env in var.deploy_environment : "env" => env}
+
+
+  name   = "${var.Vm_name}_${each.key}"
+  memory = var.VM_memory
+  vcpu   = var.Vm_vcpu
+
+  disk {
+    volume_id = libvirt_volume.vm_disk["${each.key}"].id
+  }
+
+  network_interface {
+    network_name = var.Vm_network_properties.name
+    #model       = var.Vm_network_properties.model
+  }
+
+  cloudinit = libvirt_cloudinit_disk.cloudinit.id
+
+  console {
+    type        = var.Vm_console_properties.type
+    target_type = var.Vm_console_properties.target_type
+    target_port = var.Vm_console_properties.target_port
+  }
+
+  graphics {
+    type        = var.Vm_graphics_properties.type
+    autoport    = var.Vm_graphics_properties.autoport
+    listen_type = var.Vm_graphics_properties.listen_type
+  }
+}
