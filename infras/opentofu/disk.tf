8038dd54c86a65743d8f3f070983af93473233f4
patrick <siandjipatrick@yahoo.fr>
2025-07-17 17:38:19 +0200

    clean code

--- infras/opentofu/disk.tf
+++ infras/opentofu/disk.tf
@@ -1,9 +1,9 @@
 resource "libvirt_volume" "vm_disk" {
-  for_each = local.all_vm_instances_map
+  for_each = local.all_vm_env_map
 
 
 
-  name   = "${each.key}.${var.disk_format}"
+  name   = "${var.disk_name}_${each.key}.${var.disk_format}"
   pool   = var.disk_pool
   source = var.disk_source
   format = var.disk_format
